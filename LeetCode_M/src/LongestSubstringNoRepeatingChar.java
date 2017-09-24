5cf6f16906cf3bca1d444335ab8be0bc60c18e4c
Aaron Zhang <aaronz@live.unc.edu>
2017-09-23 20:30:07 -0400

    finished longest substring

--- LeetCode_M/src/LongestSubstringNoRepeatingChar.java
+++ LeetCode_M/src/LongestSubstringNoRepeatingChar.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 public class LongestSubstringNoRepeatingChar {
     /*
     https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
@@ -12,6 +13,25 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
      */
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        if (s == null) {
+            return 0;
+        }
+        //abba
+        //Go through string find longest substring from beggining, if breaks then continue on from next char
+        HashMap<Character, Integer> charCounter = new HashMap<Character, Integer>();
+        int longestSubstring = 0;
+        int currLength;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            //string from j to i
+            if (charCounter.containsKey(s.charAt(i))) {
+                j = Math.max(charCounter.get(s.charAt(i)) + 1, j);
+                charCounter.put(s.charAt(i), i);
+            } else {
+                charCounter.put(s.charAt(i), i);
+            }
+            currLength = i - j + 1;//2
+            longestSubstring = (currLength > longestSubstring) ? currLength : longestSubstring;//2
+        }
+        return longestSubstring;
     }
 }
