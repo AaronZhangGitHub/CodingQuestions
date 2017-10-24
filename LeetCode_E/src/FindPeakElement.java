a689f2c262c422e2a1d55523f858ff03909be4a2
Aaron Zhang <aaronz@live.unc.edu>
2017-10-24 00:10:18 -0400

    finished find peak element

---
+++ LeetCode_E/src/FindPeakElement.java
@@ -0,0 +1,21 @@
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1){return 0;}
+        if(nums.length==2){return (nums[0]>nums[1])?0:1;}
+        //atleast size 3
+        int i = 0, j = 1, k = 2;
+        //Check edges
+        if(nums[i]>nums[j]){return i;}
+        if(nums[nums.length-1]>nums[nums.length-2]){return nums.length-1;}
+
+        while(k<nums.length){
+            if(nums[j]>nums[i]&&nums[j]>nums[k]){
+                return j;
+            }
+            i++;
+            j++;
+            k++;
+        }
+        return Integer.MIN_VALUE;
+    }
+}
